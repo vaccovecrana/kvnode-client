9b4f9e2f4c0eaed638df231ef35bf113d69a5f89
Jesus Zazueta <jjzazuet@vacco.io>
2021-05-29 03:29:32 -0400

    - Initial switch to kvnode. - Code cleanup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,74 +1,16 @@
-plugins {
-  `java-library`
-  `maven-publish`
-  jacoco
-}
+plugins { id("io.vacco.oss.gitflow") version "0.9.8" }
 
-group = "io.vacco.summitdb"
+group = "io.vacco.kvnode"
 version = "0.8.0"
 
-java {
-  withJavadocJar()
-  withSourcesJar()
-  sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
-  targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
+configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
+  addJ8Spec()
+  addClasspathHell()
+  sharedLibrary(true, false)
 }
 
-repositories { jcenter() }
+val api by configurations
 
 dependencies {
   api("com.github.chrisvest:stormpot:3.0")
-  testImplementation("io.github.j8spec:j8spec:3.0.0")
-}
-
-object Publishing {
-  const val gitUrl = "https://github.com/vaccovecrana/summitdb-client.git"
-  const val siteUrl = "https://github.com/vaccovecrana/summitdb-client"
-  const val libraryDesc = "SummitDB Java client"
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      from(components["java"])
-      versionMapping {
-        usage("java-api") { fromResolutionOf("runtimeClasspath") }
-        usage("java-runtime") { fromResolutionResult() }
-      }
-      pom {
-        name.set(project.name)
-        description.set(Publishing.libraryDesc)
-        url.set(Publishing.siteUrl)
-        licenses {
-          license {
-            name.set("The MIT License")
-            url.set("https://opensource.org/licenses/MIT")
-          }
-        }
-        developers {
-          developer {
-            id.set("jjzazuet")
-            name.set("Jesus Zazueta")
-            email.set("jjzazuet@vacco.io")
-          }
-        }
-        scm {
-          connection.set(Publishing.gitUrl)
-          developerConnection.set(Publishing.gitUrl)
-          url.set(Publishing.siteUrl)
-        }
-      }
-    }
-  }
-  repositories {
-    maven {
-      name = "bintray"
-      val bintrayUser = System.getenv("BINTRAY_USER")
-      setUrl("https://api.bintray.com/maven/vaccovecrana/vacco-oss/${project.name}")
-      credentials {
-        username = bintrayUser
-        password = System.getenv("BINTRAY_KEY")
-      }
-    }
-  }
 }
--- src/test/java/io/vacco/summitdb/SdLocalClient.java
+++ src/test/java/io/vacco/summitdb/SdLocalClient.java
@@ -4,7 +4,7 @@ public class SdLocalClient {
 
   public static final SdClient sc = new SdClient("127.0.0.1", 7481);
 
-  public static final void log(Object o) {
+  public static void log(Object o) {
     System.out.println(o);
   }
 }
